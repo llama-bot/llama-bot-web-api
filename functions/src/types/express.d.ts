2930ef0bc83eee83eacb60ac3f638d3a6915f964
developomp <developomp@gmail.com>
2021-09-11 18:27:10 +0900

    better type hinting

--- functions/src/types/express.d.ts
+++ functions/src/types/express.d.ts
@@ -1,10 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { auth } from "firebase-admin"
+import PassportDiscord from "passport-discord"
 
 declare global {
 	namespace Express {
-		export interface User extends auth.DecodedIdToken {
-			name?: string
+		export interface User extends auth.DecodedIdToken, PassportDiscord.Profile {
+			token: string
+			refreshToken: string
 		}
 	}
 }
